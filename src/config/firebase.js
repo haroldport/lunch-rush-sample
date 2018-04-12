574caf7cddbaf39129c52172a51d83d1f3db3e1a
Harold Portocarrero <harold.port@gmail.com>
2018-04-11 23:32:38 -0500

    firebase config properties

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+    apiKey: 'AIzaSyDeai9QaZCi5ccOwonxPhL8gfq74tduq70',
+    authDomain: 'lunch-rush-sample.firebaseapp.com',
+    databaseURL: 'https://lunch-rush-sample.firebaseio.com',
+    projectId: 'lunch-rush-sample',
+    storageBucket: 'lunch-rush-sample.appspot.com',
+    messagingSenderId: '502309617282'
 };
 
 firebase.initializeApp(config);
