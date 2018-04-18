82555acaeb2dfd00d52e95cb6c338cc7fd2b2dff
Harold Portocarrero <hportocarrero@todo1.com>
2018-04-18 17:13:54 -0500

    inclusion de libreria para cargar archivos, y componente en Restaurant basico sin integracion Firebase

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import map from 'lodash/map';
 import PropTypes from 'prop-types';
+import Files from 'react-files';
 
 import './Restaurant.css';
 
 class Restaurant extends Component {
+
+  onFilesChange = files => {
+    console.log(files);
+  };
+
+  onFilesError = (error, file) => {
+    console.log(`Error code ${error.code}: ${error.message}`);
+  };
+
   render() {
     const { name, votes, user, handleSelect, handleUnselect } = this.props;
     const userHasSelected = votes && Object.keys(votes).includes(user.uid);
@@ -24,6 +34,20 @@ class Restaurant extends Component {
               Yea, I'd go there
             </button>
         }
+        <div className="Restaurant--images">
+          <Files
+            className='files-dropzone'
+            onChange={this.onFilesChange}
+            onError={this.onFilesError}
+            accepts={['image/*']}
+            multiple={false}
+            maxFileSize={10000000}
+            minFileSize={0}
+            clickable
+          >
+            Drop files here or click to upload
+          </Files>
+        </div>
       </article>
     );
   }
