035191cc89332793b75c4c5e4ec7faf779e60d9f
Harold Portocarrero <harold.port@gmail.com>
2018-05-04 00:14:32 -0500

    vaciando config de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: 'AIzaSyDeai9QaZCi5ccOwonxPhL8gfq74tduq70',
-    authDomain: 'lunch-rush-sample.firebaseapp.com',
-    databaseURL: 'https://lunch-rush-sample.firebaseio.com',
-    projectId: 'lunch-rush-sample',
-    storageBucket: 'lunch-rush-sample.appspot.com',
-    messagingSenderId: '502309617282'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
 };
 
 firebase.initializeApp(config);
